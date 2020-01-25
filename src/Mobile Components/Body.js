493670018934790fc42a9f52b23a86c95df0e9fe
Faris Aziz <farisaziz12@gmail.com>
2020-01-25 00:48:54 +0000

    more mobile friendly

---
+++ src/Mobile Components/Body.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AboutMe from './AboutMe'
+import Skills from './Skills'
+import Languages from './Languages'
+import ContactAndSocials from './ContactAndSocials'
+
+export default function Body() {
+    return (
+        <>
+           <AboutMe/> 
+           <Skills/>
+           <Languages/>
+           <ContactAndSocials/>
+        </>
+    )
+}
---
+++ src/Mobile Components/Languages.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+
+export default function Languages() {
+    return (
+        <div className="section">
+        <div className="fixed">
+            <h1>Language I speak</h1>
+            <p>- English (Fluent)</p>
+            <p>- French (Fluent)</p>
+            <p>- Urdu (Conversational)</p>
+            <p>- Hindi (Conversational)</p>
+            <p>- German (Basic Conversational)</p>
+        </div>
+    </div>
+    )
+}
