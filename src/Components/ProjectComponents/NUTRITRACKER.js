f89285024f113c6fb8a4c10214f4ba841f48d308
Faris Aziz <farisaziz12@gmail.com>
2020-02-29 20:12:25 +0000

    text height fixed

--- src/Components/ProjectComponents/NUTRITRACKER.js
+++ src/Components/ProjectComponents/NUTRITRACKER.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PopPop from 'react-poppop';
 import './Button.css'
+import './Text.css'
 
 export default class NUTRITRACKER extends Component {
     state = {
@@ -24,7 +25,7 @@ export default class NUTRITRACKER extends Component {
                 onClose={() => this.toggleShow(false)}
                 closeOnOverlay={true}>
           <h1>Nutri-Tracker</h1>
-          <p>
+          <p className='paragraph-text'>
           Nutritracker is a web application with the simple purpose of allowing users to login, create meal plans and track calorie and macronutrient consumption to allow them to better their diet and lose weight
           </p>
           <button class="gradient-multiline-button"><a href='https://nutri-tracker.netlify.com/login'>Check out the live website</a></button>
