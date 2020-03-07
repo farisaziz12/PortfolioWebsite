99ecf3ae29ab5d047488721b92e7a5d54534412d
Faris Aziz <farisaziz12@gmail.com>
2020-03-07 15:01:12 +0000

    CF button disabled

--- src/MainComponents/Header.js
+++ src/MainComponents/Header.js
@@ -10,7 +10,7 @@ export default function Header(props) {
                     <h1 className="large">Faris Aziz</h1>
                     <p>Start scrolling :)</p>
                     <div> 
-                        <h3 style={props.type === "SE"? undefined :{backgroundColor: "#000"}} onClick={props.toggleContent} className='toggleBtn'>{props.type === "SE"? "Software Engineering" : "CrossFit" }</h3>
+                        {/* <h3 style={props.type === "SE"? undefined :{backgroundColor: "#000"}} onClick={props.toggleContent} className='toggleBtn'>{props.type === "SE"? "Software Engineering" : "CrossFit" }</h3> */}
                     </div>
                     <p><i class="i arrow down"></i></p>
             </div>
